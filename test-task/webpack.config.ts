b0bb42ee6680f756eea572eda3114910e88779f1
Осетров Сергей <saosetrov@asuproject.ru>
2025-02-06 17:48:47 +0700

    Merge branch 'main' of https://github.com/Having-no-guise/second-proj

--- test-task/webpack.config.ts
+++ test-task/webpack.config.ts
@@ -6,47 +6,57 @@ const { ModuleFederationPlugin } = require("webpack").container;
 
 import packageJson from './package.json'
 const { dependencies } = packageJson
-  mode,
-  devtool: mode === 'development' ? 'source-map' : false,
-  devServer: {
+
+export default (_: never, { mode = 'development' }: IWebpackArgs): Configuration => {
+  return {
+    mode,
+
+    devtool: mode == 'development' ? 'source-map' : false,
+    devServer: {
       static: path.resolve(__dirname, "dist"),
-    hot: true,
+      hot: true,
       port: 8081
-  },
-  entry: './src/index',
-  output: {
+    },
+
+    entry: './src/index',
+    output: {
       path: path.resolve(__dirname, "dist"),
       filename: 'catsMemsImage.js',
       publicPath: 'http://localhost:8081/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'less-loader',
-            options: { lessOptions: { javascriptEnabled: true } }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
+    },
+
+    resolve: {
+      extensions: [ '.ts', '.tsx', '.js', '.jsx' ]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'less-loader',
+              options: {
+                lessOptions: { javascriptEnabled: true }
+              }
+            }
+          ]
+        }
+      ]
+    },
+
+    plugins: [
       new ModuleFederationPlugin({
         name: 'catsImages',
         filename: 'remoteEntry.js',
@@ -62,14 +72,13 @@ const { dependencies } = packageJson
       
 
       }),
-    new HtmlWebpackPlugin({
+      new HtmlWebpackPlugin({
         title: 'test-task',
-      template: 'public/index.html'
-    })
-  ]
-});
-
-module.exports = config;
+        template: 'public/index.html'
+      })
+    ]
+  }
+}
 
 interface IWebpackArgs { mode?: 'development' | 'production' }
 
